0a3231b3d8a704ddc4be70f6f5cf3cc78d91d80a
rahularade <araderahul9196@gmail.com>
2024-08-08 16:35:45 +0530

    declare routes

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -2,7 +2,7 @@ import mongoose, { isValidObjectId, Mongoose } from "mongoose";
 import { Comment } from "../models/comment.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import { asyncHandler } from "../utils/asyncHandler";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 
 const getVideoComments = asyncHandler(async (req, res) => {
